2ac18d08e3a5fceb4d0c1fea8e6800fd320282ad
eudoxos <eu@doxos.eu>
2015-06-06 16:23:09 +0200

    Ribbed mill traingulation func
    Move from examples/mill.py to py/triangulated.py so that it is useful
    for other simulations as well.

--- examples/mill.py
+++ examples/mill.py
@@ -23,40 +23,6 @@ dTheta=pi/24 # circle division angle
 
 S=woo.master.scene=woo.core.Scene(fields=[woo.dem.DemField(gravity=(0,0,-10))])
 
-###
-### mill geometry (parameteric)
-###
-bumpPeri=2*bumpHt*tan(.5*bumpTipAngle) # length of a bump on the perimeter of the mill
-bumpAngle=bumpPeri/millRad # angle of one bump from the axis of the mill
-interBumpAngle=2*pi/bumpNum
-bumpRad=millRad-bumpHt
-pts=[]; thMin=0
-for i in range(0,bumpNum):
-	thMin+=interBumpAngle
-	thMax=thMin+interBumpAngle-bumpAngle
-	thTip=thMax+.5*bumpAngle
-	# the circular parts spanning from thMin to thMax
-	for th0 in linspace(thMin,thMax,interBumpAngle/dTheta,endpoint=True):
-		pts.append(Vector3(-.5*millDp,millRad*cos(th0),millRad*sin(th0)))
-	# tip of the bump
-	pts.append(Vector3(-.5*millDp,bumpRad*cos(thTip),bumpRad*sin(thTip)))
-# close the curve
-pts+=[pts[0]]
-# make the second contour, just shifted by millDp; ppts contains both
-ppts=[pts,[p+Vector3(millDp,0,0) for p in pts]]
-mill=woo.pack.sweptPolylines2gtsSurface(ppts,threshold=.01*min(dTheta*millRad,bumpHt))
-millPar=woo.pack.gtsSurface2Facets(mill,color=.5,wire=False) # add triangles, save their ids
-# make the caps less comfortably, but looking better as two triangle couples over the mill
-mrs2=millRad*sqrt(2)
-# cap1,cap2=[Vector3(0,0,mrs2),Vector3(0,-mrs2,0),Vector3(0,0,-mrs2)],[Vector3(0,0,mrs2),Vector3(0,0,-mrs2),Vector3(0,mrs2,0)] # 2 triangles at every side
-# for xx in -.5*millDp,.5*millDp: millPar.extend([woo.utils.facet([p+Vector3(xx,0,0) for p in cap1],color=0),woo.utils.facet([p+Vector3(xx,0,0) for p in cap2],color=0)])
-millPar+=[woo.utils.wall(-.5*millDp,axis=0,sense=+1),woo.utils.wall(.5*millDp,axis=0,sense=-1)]
-for w in millPar[-2:]: w.shape.visible=False
-centralNode=woo.core.Node(pos=(0,0,0))
-S.dem.par.addClumped(millPar,centralNode=centralNode)
-centralNode.dem.angVel=(-5,0,0)
-centralNode.dem.blocked='xyzXYZ' # addClumped does not block automatically anymore, do it by hand
-
 # define domains for initial cloud of red and blue spheres
 packHt=.8*millRad # size of the area
 bboxes=[(Vector3(-.5*millDp,-.5*packHt,-.5*packHt),Vector3(.5*millDp,0,.5*packHt)),(Vector3(-.5*millDp,0,-.5*packHt),Vector3(.5*millDp,.5*packHt,.5*packHt))]
@@ -64,9 +30,18 @@ colors=.2,.7
 for i in (0,1): # red and blue spheres
 	sp=woo.pack.SpherePack(); bb=bboxes[i]; vol=(bb[1][0]-bb[0][0])*(bb[1][1]-bb[0][1])*(bb[1][2]-bb[0][2])
 	sp.makeCloud(bb[0],bb[1],sphRad,sphRadFuzz)
-	S.dem.par.add([woo.utils.sphere(s[0],s[1],color=colors[i]) for s in sp])
-
+	S.dem.par.add([woo.dem.Sphere.make(s[0],s[1],color=colors[i]) for s in sp])
 S.dem.collectNodes()
+
+###
+### mill geometry (parameteric)
+###
+millPar,centralNode=woo.triangulated.ribbedMill((-.5*millDp,0,0),(.5*millDp,0,0),radius=millRad,majNum=bumpNum,majHt=bumpHt,majTipAngle=bumpTipAngle,div=24,wire=False,color=.5)
+millPar+=[woo.dem.Wall.make(-.5*millDp,axis=0,sense=+1),woo.dem.Wall.make(.5*millDp,axis=0,sense=-1)]
+for w in millPar[-2:]: w.shape.visible=False
+S.dem.par.addClumped(millPar,centralNode=centralNode)
+centralNode.dem.angVel=(-5,0,0)
+
 
 #print "Numer of grains",len(O.dem.par)-len(millIds)
 
--- py/triangulated.py
+++ py/triangulated.py
@@ -7,10 +7,11 @@ from woo._triangulated import *
 
 _docInlineModules=(woo._triangulated,)
 
-def cylinder(A,B,radius,div=20,axDiv=1,capA=False,capB=False,masks=None,wallCaps=False,angVel=0,fixed=True,**kw):
-	'''Return triangulated cylinder, as list of facets to be passed to :obj:`ParticleContainer.append`. ``**kw`` arguments are passed to :obj:`woo.pack.gtsSurface2Facets` (and thus to :obj:`woo.utils.facet`).
+def cylinder(A,B,radius,div=20,axDiv=1,capA=False,capB=False,masks=None,wallCaps=False,angVel=0,realRot=False,fixed=True,**kw):
+	'''Return triangulated cylinder, as list of facets to be passed to :obj:`woo.dem.ParticleContainer.append`. ``**kw`` arguments are passed to :obj:`woo.pack.gtsSurface2Facets` (and thus to :obj:`woo.utils.facet`).
 
-:param angVel: axial angular velocity of the cylinder; the cylinder has nevertheless zero velocity, only :obj:`woo.dem.Facet.fakeVel` is assigned.
+:param angVel: axial angular velocity of the cylinder
+:param realRot: if true, impose :obj:`~woo.dem.StableCircularOrbit` on all cylinder nodes; if false, only assign :obj:`woo.dem.Facet.fakeVel` is assigned. In both cases, :obj:`~woo.dem.DemData.angVel` is set on cap walls (with *wallCaps*).
 :param axDiv: divide the triangulation axially as well; the default creates facets spanning between both bases. If negative, it will attempt to have that ratio with circumferential division, e.g. specifying -1 will give nearly-square division.
 :param wallCaps: create caps as walls (with :obj:`woo.dem.wall.glAB` properly set) rather than triangulation; cylinder axis *must* be aligned with some global axis in this case, otherwise and error is raised.
 :param masks: mask values for cylinder, capA and capB respectively; it must be either *None* or a 3-tuple (regardless of whether caps are created or not); ``**kw`` can be used to specify ``mask`` which is passed to :obj:`woo.dem.Facet.make`, but *masks* will override this, if given.
@@ -50,8 +51,15 @@ def cylinder(A,B,radius,div=20,axDiv=1,capA=False,capB=False,masks=None,wallCaps
 		if capB: xxyyzz+=[[B+Vector3.Zero for yz in yyzz]]
 	ff=woo.pack.gtsSurface2Facets(woo.pack.sweptPolylines2gtsSurface(xxyyzz,threshold=min(radius,cylLen)*1e-4),fixed=fixed,**kw)
 	if angVel!=0: 
-		for f in ff:
-			f.shape.fakeVel=-radius*angVel*f.shape.getNormal().cross(ax)
+		if not realRot:
+			for f in ff:
+				f.shape.fakeVel=-radius*angVel*f.shape.getNormal().cross(ax)
+		else:
+			qq=Quaternion(); qq.setFromTwoVectors(Vector3.UnitZ,ax)
+			orbit=woo.dem.StableCircularOrbit(node=woo.core.Node(pos=A,ori=qq),omega=angVel,radius=radius)
+			for f in ff:
+				for n in f.shape.nodes:
+					n.dem.impose=orbit
 	# override masks of particles
 	if masks:
 		for f in ff: f.mask=masks[0] # set the cylinder as if everywhere first
@@ -67,6 +75,61 @@ def cylinder(A,B,radius,div=20,axDiv=1,capA=False,capB=False,masks=None,wallCaps
 					if sum([1 for n in f.shape.nodes if n.pos==B]): f.mask=masks[2]
 	return caps+ff
 
+def ribbedMill(A,B,radius,majNum,majHt,majTipAngle,minNum=0,minHt=0,minTipAngle=0,div=20,**kw):
+	'''
+
+:param majNum: number of major ribs (parallel with rotation axis)
+:param majHt: height of major ribs
+:param majTipAngle: angle on the major rib tip
+:param minNum: number of minor ribs (around the cylinder, in plane perpendicular to the rotation axis)
+:param minHt: height of minor ribs
+:param minTipAngle: angle on the minor rib tip
+:returns: tuple ``[particles],centralNode``, which can be directly used in ``S.dem.par.addClumped(*woo.triangulated.ribbedMill(...))``. ``centralNode`` is local coordinate system positioned in ``A`` and of which :math:`x`-axis is rotation axis of the mill. Assigning ``dem.angVel=centralNode.ori*(0,0,w)`` will make the mill rotate around the axis with the angular velocity ``w``. ``centralNode`` has automatically ClumpData assigned, with :obj:`~woo.dem.DemData.blocked` equal to ``xyzXYZ`.
+'''
+	A,B=Vector3(A),Vector3(B)
+	cylLen=(B-A).norm()
+	ax=(B-A)/cylLen
+	axOri=Quaternion(); axOri.setFromTwoVectors(Vector3.UnitZ,ax)
+	cs=woo.core.Node(pos=A,ori=axOri,dem=woo.dem.ClumpData(blocked='xyzXYZ'))
+	locCyl=lambda r,th,z: cs.loc2glob(woo.comp.cyl2cart(Vector3(r,th,z)))
+	locCart=cs.loc2glob
+
+	majPeri=2*majHt*math.tan(.5*majTipAngle) # length of a maj on the perimeter of the mill
+	majAngle=majPeri/radius # angle of one rib from the axis of the mill
+	interMajAngle=2*math.pi/majNum
+	interMajNum=max(2,int(math.ceil(div*1./majNum)))
+	majRad=radius-majHt
+	pts=[]; thMin=0
+	for i in range(0,majNum):
+		thMin+=interMajAngle
+		thMax=thMin+interMajAngle-majAngle
+		thTip=thMax+.5*majAngle
+		# the circular parts spanning from thMin to thMax
+		thetas=numpy.linspace(thMin,thMax,interMajNum,endpoint=True)
+		for th0 in thetas: pts.append(locCyl(radius,th0,0))
+		# tip of the major rib
+		pts.append(locCyl(majRad,thTip,0))
+	# close the curve
+	pts+=[pts[0]]
+	# make the second contour, just shifted by millDp; ppts contains both
+	ppts=[pts,[p+axOri*Vector3(0,0,cylLen) for p in pts]]
+	millPar=woo.pack.gtsSurface2Facets(woo.pack.sweptPolylines2gtsSurface(ppts,threshold=.001*majHt),**kw)
+	# minor ribs
+	minPar=[]
+	if minNum>0:
+		minHalfWd=minHt*math.tan(.5*minTipAngle)
+		thetas=numpy.linspace(0,2*math.pi,majNum+1,endpoint=True)
+		for i,xx in enumerate(numpy.linspace(0,cylLen,num=minNum,endpoint=True)):
+			pts=[]
+			for th in thetas:
+				A,B,C=locCyl(radius,th,xx-.5*minHalfWd),locCyl(radius-minHt,th,xx),locCyl(radius,th,xx+.5*minHalfWd)
+				if i==0: pts.append((B,C))
+				elif i==minNum-1: pts.append((A,B))
+				else: pts.append((A,B,C))
+			# one rib is done all around
+			minPar+=woo.pack.gtsSurface2Facets(woo.pack.sweptPolylines2gtsSurface(pts,threshold=0.001*minHt),**kw)
+	return millPar+minPar,cs
+
 def quadrilateral(A,B,C,D,size=0,div=Vector2i(0,0),**kw):
 	'Return triangulated `quadrilateral <http://en.wikipedia.org/wiki/Quadrilateral>`__ (or a `skew quadrilateral <http://en.wikipedia.org/wiki/Skew_polygon>`__), when points are not co-planar), where division size is at most *size* (absolute length) or *div* (number of subdivisions in the AB--CD and AC--BD directions).'
 	if sum(div)>0:
@@ -78,7 +141,7 @@ def quadrilateral(A,B,C,D,size=0,div=Vector2i(0,0),**kw):
 		else: div=Vector2i(2,2)
 	AB,AC,CD,BD=B-A,C-A,D-C,D-B
 	aabb,aacc=numpy.linspace(0,1,div[0]+1),numpy.linspace(0,1,div[1]+1)
-	pts=[[A+ac*AC+ab*(B+ac*BD-(A+ac*AC)).normalized() for ac in aacc] for ab in aabb]
+	pts=[[A+ac*AC+ab*(B+ac*BD-(A+ac*AC)) for ac in aacc] for ab in aabb]
 	return woo.pack.gtsSurface2Facets(woo.pack.sweptPolylines2gtsSurface(pts),**kw)
 
 def box(dim,center,**kw):
@@ -96,4 +159,30 @@ def box(dim,center,**kw):
 		for n in nn: woo.dem.DemData.setOriMassInertia(n)
 	return ff
 
+
+
+
+import woo.pyderived
+class MeshImport(woo.core.Object,woo.pyderived.PyWooObject):
+	'User interface for importing meshes from external files (STL), so that all import parameters are kept together.'
+	_classTrait=None
+	_PAT=woo.pyderived.PyAttrTrait
+	_attrTraits=[
+		_PAT(str,'file','',existingFilename=True,doc='Mesh file to be imported (STL)'),
+		_PAT(float,'preScale',1.0,doc='Scaling, applied before other transformations.'),
+		_PAT(woo.core.Node,'node',None,doc='Node defining local coordinate system for importing (already :obj:`prescaled <preScale>`) mesh; if not given, global coordinate system is assumed.'),
+		_PAT(float,'halfThick',0,unit='mm',doc='Half thickness (:obj:`woo.dem.Facet.halfThick`) assigned to all created facets. If zero, don\'t assign :obj:`~woo.dem.Facet.halfThick`.'),
+		_PAT(float,'tessMaxBox',0,unit='mm',doc='Some importers (STL) can tesselated triangles so that their bounding box dimension does not exceed :obj:`tessMaxBox`. If non-positive, no tesselation will be done.'),
+	]
+	def __init__(self,**kw):
+		woo.core.Object.__init__(self)
+		self.wooPyInit(self.__class__,woo.core.Object,**kw)
+	def doImport(self,mat,mask=woo.dem.DemField.defaultBoundaryMask):
+		fmt=None
+		if self.file.lower().endswith('.stl'): fmt='stl'
+		else: raise ValueError('Unknown mesh file extension (must be .stl).')
+		if fmt=='stl':
+			tri=woo.utils.importSTL(self.file,mat=mat,mask=mask,scale=self.preScale,shift=(self.node.pos if self.node else Vector3.Zero),ori=(self.node.ori if self.node else Quaternion.Identity),maxBox=self.tessMaxBox)
+			return tri
+		else: raise RuntimeError('Programming error: unhandled value fmt=="%s".'%(str(fmt)))
 
