643a4484025a1f90887ad5a683e48de75995e77b
eudoxos <eu@doxos.eu>
2015-07-30 17:28:49 +0200

    Add initial version of GlSetup (not yet working)

---
+++ pkg/gl/GlSetup.cpp
@@ -0,0 +1,96 @@
+#if WOO_OPENGL
+#include<woo/pkg/gl/GlSetup.hpp>
+#include<woo/pkg/gl/Functors.hpp>
+#include<woo/pkg/gl/Renderer.hpp>
+
+WOO_PLUGIN(gl,(GlSetup));
+WOO_IMPL__CLASS_BASE_DOC_ATTRS_PY(woo_gl_GlSetup__CLASS_BASE_DOC_ATTRS_PY);
+WOO_IMPL_LOGGER(GlSetup);
+
+vector<std::type_index> GlSetup::objTypeIndices;
+vector<string> GlSetup::objTypeNames;
+vector<string> GlSetup::objAccessorNames;
+
+void GlSetup::checkIndex(int i) const {
+	if(i<0 || i>=(int)objs.size()) woo::IndexError("Invalid index "+to_string(i)+" for GlSetup.objs, must be 0…"+to_string((int)objs.size()-1)+".");
+}
+
+string GlSetup::accessorName(const string&s ){
+	string ret(s);
+	if(boost::starts_with(ret,"Gl1_")) ret=ret.substr(4);
+	ret[0]=tolower(ret[0]);
+	return ret;
+}
+
+void GlSetup::postLoad(GlSetup&,void*){
+	bool ok=true;
+	if(objs.size()!=objTypeIndices.size()){ LOG_WARN("GlSetup.objs: incorrect size, falling back to defaults."); ok=false; }
+	if(ok){
+		for(size_t i=0; i<objs.size(); i++){
+			const auto oPtr=objs[i].get();
+			if((objs[i]?std::type_index(typeid(*oPtr)):std::type_index(typeid(void)))!=objTypeIndices[i]){
+				LOG_WARN("GlSetup.objs[i]: incorrect type (is "+(objs[i]?objs[i]->pyStr():"None")+", should be a "+objTypeNames[i]+", falling back to defaults.");
+				break;
+			}
+		}
+	}
+	if(!ok) objs=makeObjs();
+}
+
+void GlSetup::pyHandleCustomCtorArgs(py::tuple& args, py::dict& kw){
+	for(int i=0; i<py::len(args); i++){
+		py::extract<shared_ptr<Object>> ex(args[i]);
+		if(!ex.check()) woo::TypeError("Only instances of woo.core.Object can be given as arguments.");
+		const auto o(ex()); const auto oPtr=o.get();
+		if(!o) woo::TypeError("None not accepted as argument.");
+		auto I=std::find(objTypeIndices.begin(),objTypeIndices.end(),std::type_index(typeid(*oPtr)));
+		if(I==objTypeIndices.end()) woo::TypeError(o->pyStr()+" has type not understood by GlSetup.");
+		objs[I-objTypeIndices.begin()]=o;
+	}
+	args=py::tuple();
+	py::list kwl=kw.items();
+	for(int i=0; i<py::len(kwl); i++){
+		py::tuple item=py::extract<py::tuple>(kwl[i]);
+		string key=py::extract<string>(item[0]);
+		auto I=std::find(objAccessorNames.begin(),objAccessorNames.end(),key);
+		if(I==objAccessorNames.end()) woo::KeyError("Invalid key "+key+".)");
+		size_t index=I-objAccessorNames.begin();
+		py::extract<shared_ptr<Object>> ex(item[1]);
+		if(!ex.check()) woo::TypeError(key+" must be a "+objTypeNames[index]+".");
+		const auto o(ex()); const auto oPtr=o.get();
+		if(!o) woo::TypeError(key+" must not be None.");
+		if(std::type_index(typeid(*oPtr))!=objTypeIndices[index]) woo::TypeError(key+" must be a "+objTypeNames[index]+" (not "+o->getClassName()+").");
+		objs[index]=o;
+	}
+	kw=py::dict();
+}
+
+
+vector<shared_ptr<Object>> GlSetup::makeObjs() {
+	// functors for dispatcher types
+	std::list<shared_ptr<Object>> field,shape,bound,node,cPhys;
+	#define _PROBE_FUNCTOR(functorT,list,className) if(Master::instance().isInheritingFrom_recursive(className,#functorT)){ list.push_back(Master::instance().factorClass(className)); }
+	for(auto& item: Master::instance().getClassBases()){
+		_PROBE_FUNCTOR(GlFieldFunctor,field,item.first);
+		_PROBE_FUNCTOR(GlShapeFunctor,shape,item.first);
+		_PROBE_FUNCTOR(GlBoundFunctor,bound,item.first);
+		_PROBE_FUNCTOR(GlNodeFunctor,node,item.first);
+		_PROBE_FUNCTOR(GlCPhysFunctor,cPhys,item.first);
+	}
+	#undef _PROBE_FUNCTOR
+	vector<shared_ptr<Object>> ret;
+	ret.push_back(make_shared<Renderer>());
+	for(auto l: {&field,&shape,&bound,&node,&cPhys}){
+		if(l->empty()) continue;
+		ret.push_back(shared_ptr<Object>()); // separator
+		l->sort([](const shared_ptr<Object>& a, const shared_ptr<Object>& b){ return a->getClassName()<b->getClassName(); });
+		for(const auto& o: *l) ret.push_back(o);
+	}
+	#if 0
+	for(size_t i=0; i<ret.size(); i++){ cerr<<"GlSetup::defaultObjs: "<<i<<": "<<(ret[i]?ret[i]->getClassName():string("-"))<<endl; }
+	#endif
+	return ret;
+}
+
+
+#endif /* WOO_OPENGL */
---
+++ pkg/gl/GlSetup.hpp
@@ -0,0 +1,76 @@
+#ifdef WOO_OPENGL
+
+#include<woo/core/Scene.hpp>
+#include<woo/lib/pyutil/except.hpp>
+#include<boost/algorithm/string/predicate.hpp>
+#include<typeindex>
+
+/*
+This class tries to be very user-friendly and is convolved. Explanation and rationale follow.
+
+Objectives:
+* store all OpenGL-related settings in one class, which does not have to be modified if new rendering classes are added (only if new dispatcher types are added)
+* be user-friendly in terms of accessing all settings by their class name (or some variant thereof) as attribute of GlSetup in scripts
+* Be able to move all settings away, apply new ones and so on
+* Be able to have the class in woo.core, and make it compilable (the declarations at least) without including anything from woo.gl (therefore base everyting on woo.core.Object and cast and type-check at runtime)
+
+Technique:
+* store all classes in objs (read-only from python) and create accessors which have names derived from those objects (remove leading Gl1_, if any, and de-capitalize);
+* accessors are created as lambda functions passed to boost::python, which makes them a bit hairy
+* accessors have to check that correct type is assigned, also handling non-woo.core.Objects, None Objects
+* accessors must give nice message when an incorrect type is passed
+* correct types (as std::type_index), human-readable type-names and accessor names are remembered in objTypeIndices, objTypeNames, objAccessorNames filled at startup (when the class is registered)
+
+Interface:
+* GlSetup(..) can take both non-keyword argument (which are put where they should be, if they have correct type), and also keyword arguments, which are named the same as accessors
+* If the inial *objs* is incorrect due to mismatched types, GlSetup reverts to defaults rather than refusing the input. This is useful for backwards-compatibility.
+* individual functors can be assigned, e.g. S.gl.sphere=Gl1_Sphere(wire=True)
+* parameters of functors can be changed, e.g. S.gl.sphere.wire=True
+
+TODO:
+* when an individual functor is assigned, mark objs dirty, so that Renderer can pick them up when run the next time (some synchronization might be necessary to avoid concurrent access), or sync those right away.
+* Renderer is always the first in objs
+* Scene must keep track of the Renderer to call, Renderer must keep track of functors and scene as well
+* All currently used static data in Renderer (like fastDraw and selectedObject) must be moved somewhere inside Scene
+
+*/
+
+struct GlSetup: public Object{
+	static string accessorName(const string& s);
+	void checkIndex(int i) const;
+	static vector<shared_ptr<Object>> makeObjs(); 
+
+	static vector<std::type_index> objTypeIndices;
+	static vector<string> objTypeNames;
+	static vector<string> objAccessorNames;
+	
+	void postLoad(GlSetup&,void*);
+	void pyHandleCustomCtorArgs(py::tuple& args, py::dict& kw) WOO_CXX11_OVERRIDE;
+
+	WOO_DECL_LOGGER;
+
+	#define woo_gl_GlSetup__CLASS_BASE_DOC_ATTRS_PY \
+		GlSetup,Object,"Proxy object for accessing all GL-related settings uniformly from the scene.", \
+		((vector<shared_ptr<Object>>,objs,makeObjs(),AttrTrait<Attr::readonly>().noGui(),"List of all objects used; their order is determined at run-time. Some of them may be None (unused indices) which indicate separator in list of those objects when presented in the UI.")) \
+		,/*py*/ ; \
+			auto oo=makeObjs(); assert(objTypeindices.empty()); \
+			for(size_t i=0; i<oo.size(); i++){ \
+				const auto& o=oo[i]; const auto oPtr=o.get(); \
+				string accessor=accessorName(o?o->getClassName():""); \
+				objTypeIndices.push_back(o?std::type_index(typeid(oPtr)):std::type_index(typeid(void))); \
+				objTypeNames.push_back(o?o->getClassName():"None"); \
+				objAccessorNames.push_back(accessor); \
+				/*separator*/if(!o) continue; \
+				_classObj.add_property(accessor.c_str(),\
+					/* see https://mail.python.org/pipermail/cplusplus-sig/2009-February/014263.html and http://stackoverflow.com/questions/16845547/using-c11-lambda-as-accessor-function-in-boostpythons-add-property-get-sig */ \
+					py::detail::make_function_aux([i](shared_ptr<GlSetup> g){g->checkIndex(i); return g->objs[i];},py::default_call_policies(),boost::mpl::vector<shared_ptr<Object>,shared_ptr<GlSetup>>()), \
+					py::detail::make_function_aux([i](shared_ptr<GlSetup> g, shared_ptr<Object> val){ g->checkIndex(i); if(!val) woo::ValueError("Must not be None."); const auto valPtr=val.get(); if(std::type_index(typeid(*valPtr))!=objTypeIndices[i]) woo::TypeError(objTypeNames[i]+" instance required (not "+val->getClassName()+")."); g->objs[i]=val; },py::default_call_policies(),boost::mpl::vector<void,shared_ptr<GlSetup>,shared_ptr<Object>>()) \
+				); \
+			}
+	
+	WOO_DECL__CLASS_BASE_DOC_ATTRS_PY(woo_gl_GlSetup__CLASS_BASE_DOC_ATTRS_PY);
+
+};
+WOO_REGISTER_OBJECT(GlSetup);
+
+#endif /* WOO_OPENGL */
