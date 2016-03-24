7b20a2bf6ae6a83a8946671f2e079772ea7f297b
eudoxos <eu@doxos.eu>
2016-03-24 12:22:29 +0100

    Update Ubuntu installation script to 16.04 LTS

--- scripts/woo-install-ubuntu.sh
+++ scripts/woo-install-ubuntu.sh
@@ -5,7 +5,7 @@ echo
 echo "    1. Required packages will be installed"
 echo "    2. /usr/local is chown'ed to the current user so that we can install there without sudo"
 echo "    3. ~/woo is created/updated to the latest upstream source"
-echo "    4. ~/woo/wooExtra is populated with extra modules, which are given as arguments to the script"
+echo "    4. ~/woo/wooExtra is populated with extra modules, which are given as arguments to the script (if any)"
 echo "    5. Woo is compiled and installed."
 echo
 echo "*** You will be asked for root password several times. That is normal. ***"
@@ -13,24 +13,30 @@ echo "*** You will be asked for root password several times. That is normal. ***
 ISSUE=`cat /etc/issue`
 
 case $ISSUE in
-	*12.04*|*12.10*|*13.04*) 
-		echo '*** YOUR UBUNTU INSTALLATION IS TOO OLD, ONLY 13.10 AND NEWER ARE SUPPORTED***'
+	*Xenial*|*16.04*)
+		;;
+	*)
+		echo '*** YOUR UBUNTU INSTALLATION IS NOT RECOGNIZED, ONLY 16.04 AND NEWER ARE SUPPORTED***'
 		exit 1
-	;;
+		;;
 esac
 
-sudo apt-get install software-properties-common python-software-properties # for add-apt-repository
-sudo add-apt-repository ppa:eudoxos/minieigen
-sudo apt-get update
+# faster install
+sudo apt install eatmydata
+# install libs & headers
+sudo eatmydata apt install --yes libboost-all-dev libqt4-dev-bin libqt4-dev qt4-dev-tools libgle3-dev libqglviewer-dev-qt4 libvtk6-dev libgts-dev libeigen3-dev freeglut3-dev git ccache scons libav-tools libhdf5-serial-dev paraview
+# install python-related packages
+sudo eatmydata apt install --yes python3-all-dev python3-setuptools python3-pip python3-xlrd python3-xlsxwriter python3-numpy python3-matplotlib python3-pyqt4 python3-xlib python3-genshi python3-psutil python3-pil python3-h5py python3-lockfile python3-minieigen python3-prettytable python3-colorama ipython3 pyqt4-dev-tools python3-future
+# install python stuff that is not packaged in Ubuntu
+sudo pip3 install --system xlwt-future colour-runner
 
-sudo apt-get install libboost-all-dev python-colorama libqt4-dev-bin python-setuptools python-all-dev pyqt4-dev-tools libqt4-dev qt4-dev-tools libgle3-dev libqglviewer-dev libvtk5-dev libgts-dev libeigen3-dev freeglut3-dev python-xlrd python-xlwt python-numpy python-matplotlib python-qt4 python-xlib python-genshi python-psutil python-imaging python-h5py python-lockfile git ccache scons ipython libav-tools python-imaging python-minieigen python-prettytable
-		
+# 
 sudo chown -R $USER: /usr/local
 
 
 if [ ! -d ~/woo/.git ]; then
 	rm -rf ~/woo # in case it contains some garbage
-	git clone https://github.com/eudoxos/woodem.git ~/woo
+	git clone --depth 1 https://github.com/woodem/woo.git ~/woo
 else
 	git -C ~/woo pull
 fi
@@ -49,11 +55,11 @@ done
 # compile
 mkdir -p ~/woo-build
 ccache -M50G -F10000 # adjust maxima for ccache
-scons -C ~/woo flavor= features=gts,opengl,openmp,qt4,vtk jobs=4 buildPrefix=~/woo-build CPPPATH=`ls -d /usr/include/vtk-{5,6}.*`:/usr/include/eigen3 CXX='ccache g++' brief=0 debug=0
+scons -C ~/woo flavor= features=gts,opengl,openmp,qt4,vtk,hdf5 jobs=4 buildPrefix=~/woo-build CPPPATH=`ls -d /usr/include/vtk-6.*`:/usr/include/eigen3:/usr/include/hdf5/serial CXX='ccache g++' brief=0 debug=0 PYTHON=`which python3`
 
 ## tests
 # crash at exit perhaps, should be fixed
-woo -j4 --test || true
+woo -j4 --test
 ## test update (won't recompile, anyway)
-woo -RR -x || true 
+woo -RR -x
 echo "*** Woo has been installed successfully. ***"
