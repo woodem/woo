d58b30003912da5899ad09cc585d85c583425206
eudoxos <eu@doxos.eu>
2016-05-04 09:24:51 +0200

    Generalize ForcesToHdf5 (contact and nodal forces)

--- pkg/dem/Hdf5Export.cpp
+++ pkg/dem/Hdf5Export.cpp
@@ -1,6 +1,8 @@
-#include<woo/pkg/dem/Hdf5Export.hpp>
 #include<boost/filesystem/convenience.hpp>
+#include<woo/pkg/dem/Hdf5Export.hpp>
 #include<boost/format.hpp>
+
+#include<woo/pkg/dem/Contact.hpp>
 
 WOO_PLUGIN(dem,(DemDataTagged));
 WOO_IMPL__CLASS_BASE_DOC_ATTRS(WOO_DEM_DemDataTagged__CLASS_BASE_DOC_ATTRS);
@@ -9,11 +11,13 @@ WOO_IMPL__CLASS_BASE_DOC_ATTRS(WOO_DEM_DemDataTagged__CLASS_BASE_DOC_ATTRS);
 
 #include<H5Cpp.h>
 
-WOO_PLUGIN(dem,(NodalForcesToHdf5));
-WOO_IMPL__CLASS_BASE_DOC_ATTRS(WOO_DEM_NodalForceToHdf5__CLASS_BASE_DOC_ATTRS);
+WOO_PLUGIN(dem,(ForcesToHdf5)(NodalForcesToHdf5));
+WOO_IMPL__CLASS_BASE_DOC_ATTRS(WOO_DEM_ForceToHdf5__CLASS_BASE_DOC_ATTRS);
+WOO_IMPL__CLASS_BASE_DOC(WOO_DEM_NodalForceToHdf5__CLASS_BASE_DOC);
 
-void NodalForcesToHdf5::run(){
-	if(out.empty()) throw std::runtime_error("NodalForceToHdf5.out: empty output filename.");
+void ForcesToHdf5::run(){
+	if(out.empty()) throw std::runtime_error("ForceToHdf5.out: empty output filename.");
+	if(!(what==HDF_NODAL || what==HDF_CONTACT)) throw std::runtime_error("ForceToHdf5.what: invalid value "+to_string(what)+".");
 
 	H5::H5File h5file;
 	// open existing or create new file
@@ -22,45 +26,68 @@ void NodalForcesToHdf5::run(){
 	
 	// open/create root group for this entire simulation
 	H5::Group grp0;
-	string rootGrp=scene->expandTags("/nodalforce_{id}");
+	string rootGrp=scene->expandTags(what==HDF_NODAL?"/nodalforce_{id}":"/contactforces_{id}");
 	try{  H5::Exception::dontPrint(); grp0=h5file.openGroup(rootGrp); }
 	catch(H5::Exception& e){ grp0=h5file.createGroup(rootGrp); }
 	// create subgroup for this step
 	H5::Group grp(grp0.createGroup("step_"+(boost::format("%|06|")%scene->step).str()));
 
-	hsize_t dim[]={0,6};
+	const hsize_t nCols=6; // force+torque for nodes, force+coordinates for contacts
+	hsize_t dim[]={0,nCols};
 	hsize_t dim11[]={1};
-	// two-pass operation: first count relevant nodes, then fill lin the data
-	for(const auto& n: field->nodes){ if(n->hasData<DemData>() && n->getData<DemData>().isA<DemDataTagged>()) dim[0]++; }
-	// dim is now 2d array containing [number-of-nodes,6] (we store 2x3 components per node, force and torque)
-	H5::DataSpace fspace(2,dim); // 2d matrix Nx6
+
+	// contact matching expression (used in the first and second pass, so keep it in one place only)
+	auto contactMatch=[this](const shared_ptr<Contact>& C){ return C->isReal() && (contMask==0 || (C->leakPA()->mask&contMask) || (C->leakPB()->mask&contMask)); };
+
+	// two-pass operation: first count relevant nodes or contacts, then fill in the data
+	if(what==HDF_NODAL){ for(const auto& n: field->nodes){ if(n->hasData<DemData>() && n->getData<DemData>().isA<DemDataTagged>()) dim[0]++; } }
+	else{ for(const auto& C: *(field->cast<DemField>().contacts)){ if(contactMatch(C)) dim[0]++; } }
+
+	// dim is now 2d array containing [number-of-records,nCols] (we store nCols components per record, i.e. per node (force, torque) or per contact (force, coordinate)
+	H5::DataSpace fspace(2,dim); // 2d matrix N x nCols
 	H5::DataSpace tspace(1,dim); // 1d matrix, Nx1
-	H5::DataSpace aspace(1,dim11); // scalar (1d 1-matrix), for time value
 	// enable chunking & compression
-	hsize_t chunkdim[]={400,6}; // 400 is just a guess of what might have some efficiency for chunked storage
+	hsize_t chunkdim[]={400,nCols}; // 400 is just a guess of what might have some efficiency for chunked storage
 	H5::DSetCreatPropList plist;
 	plist.setChunk(2,chunkdim);
 	plist.setDeflate(min(max(deflate,0),9));
-	// create datasets
-	H5::DataSet fds=grp.createDataSet("forceTorque",/*type to use in the file*/H5::PredType::NATIVE_DOUBLE,fspace,plist);
-	H5::DataSet tds=grp.createDataSet("tags",H5::PredType::NATIVE_INT,tspace);
-	// write time value as forceTorque attribute
-	H5::DataSet timeDs=grp.createDataSet("time",H5::PredType::NATIVE_DOUBLE,aspace);
-	// use RowMajor, which is what HDF5 expects when passing data buffer to write (below)
-	Eigen::Matrix<double,Eigen::Dynamic,6,Eigen::RowMajor> ftarr(dim[0],6); 
-	Eigen::VectorXi tarr(dim[0]);
-	int i=0;
-	for(const auto& n: field->nodes){
-		if(!n->hasData<DemData>() || !n->getData<DemData>().isA<DemDataTagged>()) continue;
-		const auto& dyn(n->getData<DemData>().cast<DemDataTagged>());
-		ftarr.row(i).head<3>()=dyn.force;
-		ftarr.row(i).tail<3>()=dyn.torque;
-		tarr[i]=dyn.tag;
-		i+=1;
+	if(what==HDF_NODAL){
+		// create datasets
+		H5::DataSet fds=grp.createDataSet("forceTorque",/*type to use in the file*/H5::PredType::NATIVE_DOUBLE,fspace,plist);
+		H5::DataSet tds=grp.createDataSet("tags",H5::PredType::NATIVE_INT,tspace);
+		// use RowMajor, which is what HDF5 expects when passing data buffer to write (below)
+		Eigen::Matrix<double,Eigen::Dynamic,6,Eigen::RowMajor> ftarr(dim[0],6); 
+		Eigen::VectorXi tarr(dim[0]);
+		int i=0;
+		for(const auto& n: field->nodes){
+			if(!n->hasData<DemData>() || !n->getData<DemData>().isA<DemDataTagged>()) continue;
+			const auto& dyn(n->getData<DemData>().cast<DemDataTagged>());
+			ftarr.row(i).head<3>()=dyn.force;
+			ftarr.row(i).tail<3>()=dyn.torque;
+			tarr[i]=dyn.tag;
+			i+=1;
+		}
+		// write datasets, filled with Eigen data; C-style ordering (Eigen::ColumnMajor) is expected
+		fds.write(ftarr.data(),/*type of data in memory*/H5::PredType::NATIVE_DOUBLE);
+		tds.write(tarr.data(),H5::PredType::NATIVE_INT);
+	} else {
+		H5::DataSet cfds=grp.createDataSet("coordForce",/*type to use in the file*/H5::PredType::NATIVE_DOUBLE,fspace,plist);
+		Eigen::Matrix<double,Eigen::Dynamic,6,Eigen::RowMajor> cfarr(dim[0],6);
+		int i=0;
+		for(const auto& C: *(field->cast<DemField>().contacts)){
+			if(!contactMatch(C)) continue;
+			Vector3r fg=C->geom->node->loc2glob(C->phys->force); // convert to global
+			// adjust sense so that it acts in the right direction
+			if(C->leakPA()->mask&contMask) fg*=-1;
+			cfarr.row(i).head<3>()=C->geom->node->pos;
+			cfarr.row(i).tail<3>()=fg;
+			i+=1;
+		}
+		cfds.write(cfarr.data(),H5::PredType::NATIVE_DOUBLE);
 	}
-	// write datasets, filled with Eigen data; C-style ordering (Eigen::ColumnMajor) is expected
-	fds.write(ftarr.data(),/*type of data in memory*/H5::PredType::NATIVE_DOUBLE);
-	tds.write(tarr.data(),H5::PredType::NATIVE_INT);
+	// write time value as a separate 1x1 dataset
+	H5::DataSpace aspace(1,dim11); // scalar (1d 1-matrix), for time value
+	H5::DataSet timeDs=grp.createDataSet("time",H5::PredType::NATIVE_DOUBLE,aspace);
 	timeDs.write(&scene->time,H5::PredType::NATIVE_DOUBLE);
 	// close everything
 	grp.close();
--- pkg/dem/Hdf5Export.hpp
+++ pkg/dem/Hdf5Export.hpp
@@ -5,20 +5,29 @@
 
 // not HDF5-specific (but not used elsewhere, either)
 struct DemDataTagged: public DemData{
-	#define WOO_DEM_DemDataTagged__CLASS_BASE_DOC_ATTRS DemDataTagged,DemData,"Add integer tag to each node, used with :obj:`NodalForcesToHdf5` so that nodes can be numbered differently than in Woo.", \
+	#define WOO_DEM_DemDataTagged__CLASS_BASE_DOC_ATTRS DemDataTagged,DemData,"Add integer tag to each node, used with :obj:`ForcesToHdf5` so that nodes can be numbered differently than in Woo.", \
 		((int,tag,-1,,"External identifier of th node (unused by Woo itself)."))
 	WOO_DECL__CLASS_BASE_DOC_ATTRS(WOO_DEM_DemDataTagged__CLASS_BASE_DOC_ATTRS);
 };
 WOO_REGISTER_OBJECT(DemDataTagged);
 
 #ifdef WOO_HDF5
-struct NodalForcesToHdf5: public PeriodicEngine{
+struct ForcesToHdf5: public PeriodicEngine{
 	bool acceptsField(Field* f) override { return dynamic_cast<DemField*>(f); }
 	void run() override;
-	#define WOO_DEM_NodalForceToHdf5__CLASS_BASE_DOC_ATTRS NodalForcesToHdf5,PeriodicEngine,"Periodically export forces acting on nodes to HDF5 file. Only processes tagged nodes (having :obj:`DemDataTagged` attached, instead of plain :obj:`DemData`).", \
+	enum{ HDF_NODAL, HDF_CONTACT};
+	#define WOO_DEM_ForceToHdf5__CLASS_BASE_DOC_ATTRS ForcesToHdf5,PeriodicEngine,"Periodically export nodal/contact forces to HDF5 file. Nodal forces are only exported for tagged nodes (having :obj:`DemDataTagged` attached, instead of plain :obj:`DemData`) and include both force and torque. Contact forces only export force (not torque, which is zero for most models, but the adaptation would be easy).", \
+		((int,what,HDF_NODAL,AttrTrait<Attr::namedEnum>().namedEnum({{HDF_NODAL,{"node","nodes","nodal"}},{HDF_CONTACT,{"contact","contacts"}}}),"Select whether to export nodal forces (default) or contact forces.")) \
+		((int,contMask,DemField::defaultLoneMask,,"Only export contacts where at least one particle matches this mask. If zero, match all contacts. If this :obj:`Contact.pA` has matching mask, inverts force sense; this has as result that (unless both particles match) the force is exported as it acts on the matching particle.")) \
 		((string,out,"",,"Name of the output file.")) \
 		((int,deflate,9,,"Compression level for HDF5 chunked storage; valid values are 0 to 9 (will be clamped if outside)."))
-	WOO_DECL__CLASS_BASE_DOC_ATTRS(WOO_DEM_NodalForceToHdf5__CLASS_BASE_DOC_ATTRS);
+	WOO_DECL__CLASS_BASE_DOC_ATTRS(WOO_DEM_ForceToHdf5__CLASS_BASE_DOC_ATTRS);
+};
+WOO_REGISTER_OBJECT(ForcesToHdf5);
+
+struct NodalForcesToHdf5: public ForcesToHdf5{
+	#define WOO_DEM_NodalForceToHdf5__CLASS_BASE_DOC NodalForcesToHdf5,ForcesToHdf5,"Legacy class which only serves for backwards compatibility."
+	WOO_DECL__CLASS_BASE_DOC(WOO_DEM_NodalForceToHdf5__CLASS_BASE_DOC);
 };
 WOO_REGISTER_OBJECT(NodalForcesToHdf5);
 #endif /* WOO_HDF5 */
