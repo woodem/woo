789962480adb1531d7f9241788c4786da85d5206
eudoxos <eu@doxos.eu>
2017-03-22 10:11:48 +0100

    Luding model: improve/fix energy tracking

--- pkg/dem/Luding.cpp
+++ pkg/dem/Luding.cpp
@@ -2,7 +2,7 @@
 WOO_PLUGIN(dem,(LudingMat)(LudingMatState)(LudingPhys)(Cp2_LudingMat_LudingPhys)(Law2_L6Geom_LudingPhys));
 WOO_IMPL__CLASS_BASE_DOC_ATTRS(woo_dem_LudingMat__CLASS_BASE_DOC_ATTRS);
 WOO_IMPL__CLASS_BASE_DOC_ATTRS(woo_dem_LudingMatState__CLASS_BASE_DOC_ATTRS);
-WOO_IMPL__CLASS_BASE_DOC_ATTRS(woo_dem_LudingPhys__CLASS_BASE_DOC_ATTRS);
+WOO_IMPL__CLASS_BASE_DOC_ATTRS_PY(woo_dem_LudingPhys__CLASS_BASE_DOC_ATTRS_PY);
 WOO_IMPL__CLASS_BASE_DOC_ATTRS(woo_dem_Cp2_LudingMat_LudingPhys__CLASS_BASE_DOC_ATTRS);
 WOO_IMPL__CLASS_BASE_DOC_ATTRS(woo_dem_Law2_L6Geom_LudingPhys__CLASS_BASE_DOC_ATTRS);
 
@@ -44,54 +44,95 @@ void Cp2_LudingMat_LudingPhys::go(const shared_ptr<Material>& mat1, const shared
 	ph.statW=min(m1.statW,m2.statW);
 }
 
-void LudingPhys::addWork(int type, const Real& dW){
-	assert(type>=0 && type<WORK_SIZE);
-	if(work.size()<WORK_SIZE) work.resize(WORK_SIZE,0.);
-	work[type]+=dW;
+
+void Law2_L6Geom_LudingPhys::addWork(LudingPhys& ph, int type, const Real& dW) {
+	assert(type>=0 && type<LudingPhys::WORK_SIZE);
+	if(ph.work.size()<LudingPhys::WORK_SIZE) ph.work.resize(LudingPhys::WORK_SIZE,0.);
+	ph.work[type]+=dW;
+	if(wImmediate){
+		switch(type){
+			case LudingPhys::WORK_VISCOUS: scene->energy->add(dW,"viscous",viscIx,EnergyTracker::IsIncrement | EnergyTracker::ZeroDontCreate); break;
+			case LudingPhys::WORK_PLASTIC: scene->energy->add(dW,"plastic",plastIx,EnergyTracker::IsIncrement | EnergyTracker::ZeroDontCreate); break;
+		}
+	}
 }
 
-void LudingPhys::commitWork(const Scene* scene, const Particle* pA, const Particle* pB, int& viscIx, int& plastIx) const {
+void Law2_L6Geom_LudingPhys::commitWork(const shared_ptr<Contact>& C) {
+	auto& ph(C->phys->cast<LudingPhys>());
 	assert(scene->trackEnergy);
-	if(work.size()<WORK_SIZE) return;
-	scene->energy->add(work[WORK_VISCOUS],"viscous",viscIx,EnergyTracker::IsIncrement | EnergyTracker::ZeroDontCreate);
-	scene->energy->add(work[WORK_PLASTIC],"plastic",plastIx,EnergyTracker::IsIncrement | EnergyTracker::ZeroDontCreate);
+	if(ph.work.size()<LudingPhys::WORK_SIZE) return;
+	// in immediate mode, this was already done in addWork
+	if(!wImmediate){
+		scene->energy->add(ph.work[LudingPhys::WORK_VISCOUS],"viscous",viscIx,EnergyTracker::IsIncrement | EnergyTracker::ZeroDontCreate);
+		scene->energy->add(ph.work[LudingPhys::WORK_PLASTIC],"plastic",plastIx,EnergyTracker::IsIncrement | EnergyTracker::ZeroDontCreate);
+	}
+	const Particle* pA(C->leakPA());
+	const Particle* pB(C->leakPB());
 	for(const Particle* p: {pA,pB}){
 		if(!p->matState) continue;
 		boost::mutex::scoped_lock l(p->matState->lock);
 		assert(p->matState->isA<LudingMatState>());
 		auto& lms=p->matState->cast<LudingMatState>();
 		// each particle gets only half
-		lms.total+=.5*(work[WORK_VISCOUS]+work[WORK_PLASTIC]);
-		lms.visco+=.5*(work[WORK_VISCOUS]);
-		lms.plast+=.5*(work[WORK_PLASTIC]);
+		lms.total+=.5*(ph.work[LudingPhys::WORK_VISCOUS]+ph.work[LudingPhys::WORK_PLASTIC]);
+		lms.visco+=.5*(ph.work[LudingPhys::WORK_VISCOUS]);
+		lms.plast+=.5*(ph.work[LudingPhys::WORK_PLASTIC]);
 	}
 }
 
 
 // use 3 different types to accomodate combinations of Vector2r, Eigen::Map<Vector2r> etc
 template<typename VecT1, typename VecT2, typename VecT3>
-void Luding_genericSlidingRoutine(const Real& yieldLim, const Real& k, const Real& visc, const VecT1& vel, VecT2& xi, const Real& dt, const Real& dynDivStat, VecT3& result, bool trackEnergy, LudingPhys& ph){
+void Luding_genericSlidingRoutine(Law2_L6Geom_LudingPhys* law, const Real& yieldLim, const Real& k, const Real& visc, const VecT1& vel, VecT2& xi, const Real& dynDivStat, VecT3& resultF, LudingPhys& ph){
 	static_assert(std::is_same<typename VecT1::Scalar,Real>::value,"Scalar type must be Real");
+	const Real& dt(law->scene->dt);
+	const bool& trackEnergy(law->scene->trackEnergy);
 	// compatible plain dense vector type
 	typedef Eigen::Matrix<typename VecT1::Scalar,VecT1::RowsAtCompileTime,1> VecT;
+	Real xi0sq(xi.squaredNorm());
 	// trial force
 	VecT ft=-k*xi-visc*vel;
-	// viscous dissipation
-	if(unlikely(trackEnergy)) ph.addWork(LudingPhys::WORK_VISCOUS,visc*vel.squaredNorm()*dt);
 	if(ft.squaredNorm()<=yieldLim*yieldLim){
 		// eq(19), static friction
 		xi+=vel*dt;
-		result=ft;
-		// no plastic dissipation
+		resultF=ft;
+		// viscous dissipation, only without slip
+		if(unlikely(trackEnergy)){
+			law->addWork(ph,LudingPhys::WORK_VISCOUS,visc*vel.squaredNorm()*dt);
+		}
 	} else {
 		// sliding (dynamic) friction norm
 		Real fd=yieldLim*dynDivStat;
 		Real ftNorm=ft.norm();
-		result=(ft/ftNorm)*fd;
-		xi=(-1/k)*(result+visc*vel);
-		// compute dissipation
-	   if(unlikely(trackEnergy)) ph.addWork(LudingPhys::WORK_PLASTIC,(1./2)*(ftNorm+fd)*(1./k)*(ftNorm-fd));
+		resultF=(ft/ftNorm)*fd;
+		if(k>0){
+			// only meaningful with non-zero stiffness
+			VecT2 xiPrev=xi;
+			xi=(-1/k)*(resultF+visc*vel);
+			// compute dissipation
+		   if(unlikely(trackEnergy)){
+				// XXX: sometimes bogus negative value??
+				Real W=xiPrev.squaredNorm()-xi.squaredNorm();
+				//if(W>0) law->addWork(ph,LudingPhys::WORK_PLASTIC,(1./2)*k*W);
+				//if(xiPrev.squaredNorm()<xi.squaredNorm()) cerr<<"<";
+
+
+				// law->addWork(ph,LudingPhys::WORK_PLASTIC,(1./2)*(ftNorm+fd)*(1./k)*(ftNorm-fd));
+				// only consider the plastic part of dissipation; the viscous sliding is fictious
+				//VecT f0=k*xiPrev;
+				//if(f0.squaredNorm()>yieldLim*yieldLim){
+				//	ph.addWork(LudingPhys::WORK_PLASTIC,(1./2)*(1./k)*(f0.squaredNorm()-pow(fd,2)));
+				//	}
+				//VecT f0=k*xiPrev, f1=k*xi;
+				//ph.addWork(LudingPhys::WORK_PLASTIC,(1./2)*k*(f0+f1).norm()*(f0-f1).norm());
+			}
+		} else {
+			xi=VecT2::Zero();
+		}
 	}
+	Real xi1sq(xi.squaredNorm());
+	// if(xi1sq<
+	//ph.addWork(LudingPhys::WORK_PLASTIC,(1./2)*k*(xi1sq-xi0sq));
 }
 
 
@@ -109,8 +150,6 @@ bool Law2_L6Geom_LudingPhys::go(const shared_ptr<CGeom>& cg, const shared_ptr<CP
 	typedef Eigen::Map<Eigen::Matrix<Real,1,1>> Map1r;
 	Map2r velT(&g.vel[1]);
 	Map2r angVelR(&g.angVel[1]);
-	//const Real& angVelW(g.angVel[0]);
-	//Map1r angVelW_vec1(&g.angVel[1]);
 
 	if(g.uN>0){
 		// breaking contact, commit dissipation data now
@@ -118,11 +157,11 @@ bool Law2_L6Geom_LudingPhys::go(const shared_ptr<CGeom>& cg, const shared_ptr<CP
 			// compute normal plastic work (non-incremental)
 			// use previous kn2 and deltaMax, those won't be increased in the step the contact is broken
 			const Real& kn2(ph.kn);
-			Real delta0=ph.deltaMax*(ph.kn1/kn2);
+			Real delta0=min(ph.deltaLim,ph.deltaMax)*(1-ph.kn1/kn2);
 			Real deltaMin=delta0*kn2/(kn2+ph.kna);
 			Real Wnp=/*W_np*/(ph.kn1*ph.deltaMax+ph.kna*deltaMin)*delta0;
-			// ph.addWork(LudingPhys::WORK_PLASTIC,Wnp);
-			ph.commitWork(scene,C->leakPA(),C->leakPB(),viscIx,plastIx);
+			addWork(ph,LudingPhys::WORK_PLASTIC,Wnp);
+			commitWork(C);
 			// just in case
 			//ph.work[LudingPhys::WORK_VISCOUS]=ph.work[LudingPhys::WORK_PLASTIC]=0.;
 		}
@@ -142,7 +181,7 @@ bool Law2_L6Geom_LudingPhys::go(const shared_ptr<CGeom>& cg, const shared_ptr<CP
 	k2=(ph.deltaMax>=ph.deltaLim)?k2hat:(k1+(k2hat-k1)*ph.deltaMax/ph.deltaLim);
 	// cerr<<"k2="<<k2<<", a12="<<a12<<endl;
 	// in text after eq (6)
-	Real delta0=(1-k1/k2)*ph.deltaMax;
+	Real delta0=(1-k1/k2)*ph.deltaMax; // XXX: min(ph.deltaLim,ph.deltaMax)
 	// eq (6)
 	Real fHys=NaN;
 	Real k2dd0=k2*(delta-delta0);
@@ -160,28 +199,28 @@ bool Law2_L6Geom_LudingPhys::go(const shared_ptr<CGeom>& cg, const shared_ptr<CP
 	}
 	assert(!isnan(fHys));
 	// after eq (8); final normal force
-	Real fn=fHys+ph.viscN*(-1.)*velN; // XXX: velN sign: invert
+	Real fn=fHys+ph.viscN*(-1.)*velN;
 	// force: viscN*velN; distance: velN*Δt
-	if(unlikely(scene->trackEnergy)) ph.addWork(LudingPhys::WORK_VISCOUS,ph.viscN*velN*velN*scene->dt);
+	if(unlikely(scene->trackEnergy)) addWork(ph,LudingPhys::WORK_VISCOUS,ph.viscN*velN*velN*scene->dt);
 	// plastic work computed when contact dissolves
 
 	// TANGENT
 	Vector2r ft;
 	// coulomb sliding (yield) force
 	Real fts=ph.tanPhi*(fn+ph.kna*delta);
-	Luding_genericSlidingRoutine(fts,ph.kt,ph.viscT,velT,ph.xiT,scene->dt,ph.dynDivStat,ft,scene->trackEnergy,ph);
+	Luding_genericSlidingRoutine(this,fts,ph.kt,ph.viscT,velT,ph.xiT,ph.dynDivStat,ft,ph);
 
 	// ROLL
 	Vector2r mr;
 	Real mrs=ph.statR*(fn+ph.kna*delta)*a12;
-	Luding_genericSlidingRoutine(mrs,ph.kr,ph.viscR,angVelR,ph.xiR,scene->dt,ph.dynDivStat,mr,scene->trackEnergy,ph);
+	Luding_genericSlidingRoutine(this,mrs,ph.kr,ph.viscR,angVelR,ph.xiR,ph.dynDivStat,mr,ph);
 
 	// TWIST
 	Real mw;
 	Map1r mw_vec(&mw);
 	Real mws=ph.statW*(fn+ph.kna*delta)*a12;
 	Map1r xiW_vec(&ph.xiW);
-	Luding_genericSlidingRoutine(mws,ph.kw,ph.viscW,Map1r_const(&g.angVel[0]),xiW_vec,scene->dt,ph.dynDivStat,mw_vec,scene->trackEnergy,ph);
+	Luding_genericSlidingRoutine(this,mws,ph.kw,ph.viscW,Map1r_const(&g.angVel[0]),xiW_vec,ph.dynDivStat,mw_vec,ph);
 
 	// invert signes for applied forces
 	Fn=-fn;
--- pkg/dem/Luding.hpp
+++ pkg/dem/Luding.hpp
@@ -54,9 +54,9 @@ WOO_REGISTER_OBJECT(LudingMatState);
 
 struct LudingPhys: public FrictPhys{
 	enum{WORK_PLASTIC=0,WORK_VISCOUS=1,WORK_SIZE=2};
-	void addWork(int type, const Real& dW);
-	void commitWork(const Scene* scene, const Particle* pA, const Particle* pB, int& viscIx, int& plastIx) const;
-	#define woo_dem_LudingPhys__CLASS_BASE_DOC_ATTRS \
+	Real Wplast()const{ return work.size()==WORK_SIZE?work[WORK_PLASTIC]:NaN; }
+	Real Wvisc()const{ return work.size()==WORK_SIZE?work[WORK_VISCOUS]:NaN; }
+	#define woo_dem_LudingPhys__CLASS_BASE_DOC_ATTRS_PY \
 		LudingPhys,FrictPhys,"Physical properties for :ref:`luding-contact-model`.", \
 			/* FrictPhys: kn≡k2 (as current elastic stiffness), kt, tanPhi */ \
 			((Real,kn1,NaN,,"Normal plastic (loading) stiffness."))\
@@ -76,9 +76,10 @@ struct LudingPhys: public FrictPhys{
 			((Vector2r,xiT,Vector2r::Zero(),,"Tangent elastic displacement.")) \
 			((Vector2r,xiR,Vector2r::Zero(),,"Tangent elastic rotation.")) \
 			((Real,xiW,0.,,"Twist elastic rotation.")) \
-			((vector<Real>,work,,,"Per-contact dissipation (unallocated when energy tracking is not enabled).")) 
+			((vector<Real>,work,,,"Per-contact dissipation (unallocated when energy tracking is not enabled).")) \
+			,/*py*/ .def_readonly("Wplast",&LudingPhys::Wplast).def_readonly("Wvisc",&LudingPhys::Wvisc)
 
-	WOO_DECL__CLASS_BASE_DOC_ATTRS(woo_dem_LudingPhys__CLASS_BASE_DOC_ATTRS);
+	WOO_DECL__CLASS_BASE_DOC_ATTRS_PY(woo_dem_LudingPhys__CLASS_BASE_DOC_ATTRS_PY);
 };
 WOO_REGISTER_OBJECT(LudingPhys);
 
@@ -94,10 +95,14 @@ WOO_REGISTER_OBJECT(Cp2_LudingMat_LudingPhys);
 
 struct Law2_L6Geom_LudingPhys: public LawFunctor{
 	bool go(const shared_ptr<CGeom>&, const shared_ptr<CPhys>&, const shared_ptr<Contact>&) override;
+	void addWork(LudingPhys& ph, int type, const Real& dW);
+	void commitWork(const shared_ptr<Contact>& C);
+
 	FUNCTOR2D(L6Geom,LudingPhys);
 	WOO_DECL_LOGGER;
 	#define woo_dem_Law2_L6Geom_LudingPhys__CLASS_BASE_DOC_ATTRS \
 		Law2_L6Geom_LudingPhys,LawFunctor,"Contact law implementing :ref:`luding-contact-model`.", \
+		((bool,wImmediate,true,,"Increment plastic & viscous work in :obj:`S.energy <woo.core.Scene.EnergyTracker>` in every step rather than commiting the sum when contact dissolves. Note that :obj:`Particle.matState <woo.dem.MatState>` (if a :obj:`LudingMatState`) is always updated only when the contact dissolves.")) \
 		((int,viscIx,-1,AttrTrait<Attr::noSave|Attr::hidden>(),"Index of viscous dissipation.")) \
 		((int,plastIx,-1,AttrTrait<Attr::noSave|Attr::hidden>(),"Index of plastic dissipation."))
 	WOO_DECL__CLASS_BASE_DOC_ATTRS(woo_dem_Law2_L6Geom_LudingPhys__CLASS_BASE_DOC_ATTRS);
