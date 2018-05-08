82dd9ca708041a6af3bfe87e3f7ff1c57cddc2c2
eudoxos <eu@doxos.eu>
2018-05-08 10:19:18 +0200

    Update class names in examples for Hertz model (closes #28)
    Cp2_FrictMat_HertzPhys -> Cp2_HertzMat_HertzPhys plus params moving to
    HertzMat.
    
    Thanks to @japaf for reporting.

--- examples/hertz-fall.py
+++ examples/hertz-fall.py
@@ -4,8 +4,8 @@ from woo.core import *
 from woo.dem import *
 from woo import plot
 import woo.log
-woo.master.usesApi=10101
-m=FrictMat(density=1e3,young=1e7,ktDivKn=.2,tanPhi=.5)
+woo.master.usesApi=10104
+m=HertzMat(density=1e3,young=1e7,ktDivKn=.2,tanPhi=.5,surfEnergy=0.,)
 S=woo.master.scene=Scene(fields=[DemField(gravity=(0,0,-10))])
 S.dtSafety=0.1
 S.dem.par.add([
@@ -14,7 +14,7 @@ S.dem.par.add([
 ])
 # S.dem.par[1].vel=(0,0,-1)
 S.dtSafety=0.06
-S.engines=utils.defaultEngines(damping=.0,cp2=Cp2_FrictMat_HertzPhys(gamma=0.,en=1.,label='cp2'),law=Law2_L6Geom_HertzPhys_DMT(noAttraction=True),dynDtPeriod=10)+[
+S.engines=utils.defaultEngines(damping=.0,cp2=Cp2_HertzMat_HertzPhys(en=1.,label='cp2'),law=Law2_L6Geom_HertzPhys_DMT(noAttraction=True),dynDtPeriod=10)+[
     LawTester(ids=(0,1),abWeight=1.,label='tester',stages=[LawTesterStage(values=(0,0,0,0,0,0),whats='......',until='False',
         #,until='stage.rebound'
     )],done='tester.restart(); S.stop()'),
