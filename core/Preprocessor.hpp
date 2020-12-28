5e85e82dfd05af839d2364fec27cc5e3de023e28
eudoxos <eu@doxos.eu>
2020-12-28 09:13:33 +0100

    Add Preprocessor.title (so that preprocessor can set title when not in batch)

--- core/Preprocessor.hpp
+++ core/Preprocessor.hpp
@@ -6,7 +6,9 @@ struct Preprocessor: public Object{
 	virtual shared_ptr<Scene> operator()();
 	#define woo_core_Preprocessor_CLASS_BASE_DOC_ATTRS_PY \
 		Preprocessor,Object,"Subclasses of this class generate a Scene object when called, based on their attributes." \
-		, /* attrs */ , /* py */ .def("__call__",&Preprocessor::operator())
+		, /* attrs */ \
+		((string,title,"",,"Simulation title, will be assigned to the {title} tag, unless running in batch (where the batch title will be used.")) \
+		, /* py */ .def("__call__",&Preprocessor::operator())
 	WOO_DECL__CLASS_BASE_DOC_ATTRS_PY(woo_core_Preprocessor_CLASS_BASE_DOC_ATTRS_PY);
 };
 WOO_REGISTER_OBJECT(Preprocessor);
