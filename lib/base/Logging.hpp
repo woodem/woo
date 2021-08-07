1b1d1cc433914b0a3531eae7a6912852469f2047
Václav Šmilauer <eu@doxos.eu>
2021-08-07 14:07:03 +0200

    vrackets around logging macros (necessary?)

--- lib/base/Logging.hpp
+++ lib/base/Logging.hpp
@@ -41,12 +41,12 @@
 #include<woo/lib/spdlog/sinks/stdout_color_sinks.h>
 #include<woo/lib/spdlog/fmt/ostr.h>
 
-#define LOG_TRACE(...) SPDLOG_LOGGER_TRACE(logger,__VA_ARGS__)
-#define LOG_DEBUG(...) SPDLOG_LOGGER_DEBUG(logger,__VA_ARGS__)
-#define LOG_INFO(...) SPDLOG_LOGGER_INFO(logger,__VA_ARGS__)
-#define LOG_WARN(...) SPDLOG_LOGGER_WARN(logger,__VA_ARGS__)
-#define LOG_ERROR(...) SPDLOG_LOGGER_ERROR(logger,__VA_ARGS__)
-#define LOG_FATAL(...) SPDLOG_LOGGER_CRITICAL(logger,__VA_ARGS__)
+#define LOG_TRACE(...) {SPDLOG_LOGGER_TRACE(logger,__VA_ARGS__);}
+#define LOG_DEBUG(...) {SPDLOG_LOGGER_DEBUG(logger,__VA_ARGS__);}
+#define LOG_INFO(...)  {SPDLOG_LOGGER_INFO(logger,__VA_ARGS__);}
+#define LOG_WARN(...)  {SPDLOG_LOGGER_WARN(logger,__VA_ARGS__);}
+#define LOG_ERROR(...) {SPDLOG_LOGGER_ERROR(logger,__VA_ARGS__);}
+#define LOG_FATAL(...) {SPDLOG_LOGGER_CRITICAL(logger,__VA_ARGS__);}
 
 #define WOO_DECL_LOGGER public: static std::shared_ptr<spdlog::logger> logger;
 #define WOO_IMPL_LOGGER(classname) std::shared_ptr<spdlog::logger> classname::logger=spdlog::stdout_color_mt(#classname)
