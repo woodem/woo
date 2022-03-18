bed39d6f25d4c6bc7fa8750ca8f64afee15e6c62
eudoxos <github@doxos.eu>
2022-03-18 15:08:31 +0000

    Deploying to gh-pages from @ woodem/woo@f6fbd0c1fa171523d9174ae16f8f6bf2a40c32ed 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0+rev1-git-681f22d',
+    VERSION: '1.0+rev1-git-f6fbd0c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
