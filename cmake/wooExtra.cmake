cd5c81a01c605b812cb25afe0d2d3667833fb3c5
Václav Šmilauer <eu@doxos.eu>
2024-11-28 09:41:15 +0100

    CMake updates/fixes (for newer distributions)

--- cmake/wooExtra.cmake
+++ cmake/wooExtra.cmake
@@ -2,13 +2,9 @@ message(STATUS "Installing @CMAKE_SOURCE_DIR@/wooExtra/*")
 file(GLOB WOOEXTRA_SETUPS @CMAKE_SOURCE_DIR@/wooExtra/*/setup.py)
 foreach(SETUP ${WOOEXTRA_SETUPS})
 	get_filename_component(SETUP_DIR ${SETUP} DIRECTORY)
-	# call with --user only if not in a virtual environment (test env var VIRTUAL_ENV)
-	if(NOT DEFINED ENV{VIRTUAL_ENV})
-		set(SETUPPY_EXTRA "--user")
-	endif()
-	message(STATUS "in ${SETUP_DIR}: @PYTHON_EXECUTABLE@ -W ignore '${SETUP}' --quiet install ${SETUPPY_EXTRA}")
+	message(STATUS "in ${SETUP_DIR}: @PYTHON_EXECUTABLE@ -W ignore -m pip '${SETUP}' --quiet install @SETUPPY_EXTRA@ .")
 	execute_process(
-		COMMAND @PYTHON_EXECUTABLE@ -W ignore "${SETUP}" --quiet install ${SETUPPY_EXTRA}
+		COMMAND @PYTHON_EXECUTABLE@ -W ignore -m pip install @SETUPPY_EXTRA@ .
 		WORKING_DIRECTORY ${SETUP_DIR}
 		RESULT_VARIABLE SETUP_STATUS
 		OUTPUT_VARIABLE SETUP_OUTPUT
